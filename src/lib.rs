3d4ff89af669f5abfeea9bd83f79fa8766a2a88a
Andrej Shadura <andrew.shadura@collabora.co.uk>
2021-10-05 09:53:38 +0200

    Explicitly cast the mode to mode_t
    On FreeBSD, mode_t is u16, not u32; we only case about file modes,
    so truncating behaviour of the type cast is okay in this case.
    
    Fixes: #4

--- src/lib.rs
+++ src/lib.rs
@@ -121,7 +121,7 @@ impl Pidfile {
             Ok(c_path) => {
                 let mut old_pid: pid_t = -1;
                 let pidfn = unsafe {
-                    bsd_pidfile_open(c_path.as_ptr(), permissions.mode(), &mut old_pid)
+                    bsd_pidfile_open(c_path.as_ptr(), permissions.mode() as mode_t, &mut old_pid)
                 };
                 if !pidfn.is_null() {
                     return Ok(Pidfile {
