2fcb45415255288f3635159a132a22810265601a
Andrej Shadura <andrew.shadura@collabora.co.uk>
2021-10-05 16:05:20 +0200

    Use system-deps to declare dependencies in Cargo.toml

--- build.rs
+++ build.rs
@@ -1,6 +1,3 @@
 fn main() {
-    if pkg_config::Config::new().probe("libbsd").is_ok() {
-        println!("cargo:rustc-cfg=pidfile");
-    }
-    println!("cargo:rerun-if-changed=build.rs");
+    system_deps::Config::new().probe().unwrap();
 }
