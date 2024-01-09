7680fe654f037e6e35949ebddc72fb7339766ed1
Alan Somers <asomers@gmail.com>
2024-01-08 17:39:27 -0700

    Fix the build on FreeBSD
    And DragonflyBSD too, probably (but not tested).

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
+#[cfg(any(target_os = "dragonfly", target_os = "freebsd"))]
+fn main() {
+    println!("cargo:rustc-link-lib=util");
+    println!("cargo:rerun-if-changed=build.rs");
+}
+
+#[cfg(not(any(target_os = "dragonfly", target_os = "freebsd")))]
 fn main() {
     system_deps::Config::new().probe().unwrap();
 }
